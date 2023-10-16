7cc7b7786cd85ef71f6dc050f07662645c043c5b
mpacdia <146739066+mpacdia@users.noreply.github.com>
2023-10-16 16:41:08 +0100

    nada

--- Assets/Scripts/selfRotateCoin.cs
+++ Assets/Scripts/selfRotateCoin.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class selfRotateCoin : MonoBehaviour
-{
-
+{ 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.forward, 85 * Time.deltaTime, Space.Self);
+
     }
 }
